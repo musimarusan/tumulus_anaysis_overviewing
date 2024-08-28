aa8caae7c19927d160270649a20384c09a95a914
musimarusan <hiroakit@me.com>
2024-08-28 21:41:10 +0900

    newly created ml_create_file_list.py

---
+++ src/ml_create_file_list.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+
+def main(path: str):
+
+    # Train
+    train_true  = os.listdir(Train_True)
+    df_tr_true = pd.DataFrame(train_true, columns=['file_name'])
+    df_tr_true['flag'] = 1
+
+    train_false = os.listdir(Train_False)
+    df_tr_false = pd.DataFrame(train_false, columns=['file_name'])
+    df_tr_false ['flag'] = 0
+
+    df = pd.concat([df_tr_true, df_tr_false])
+
+    csv = f'{path}/train_data.csv'
+    df.to_csv(csv)
+
+    # Test
+    test_true  = os.listdir(Test_True)
+    df_te_true = pd.DataFrame(test_true, columns=['file_name'])
+    df_te_true['flag'] = 1
+
+    test_false = os.listdir(Test_False)
+    df_te_false = pd.DataFrame(test_false, columns=['file_name'])
+    df_te_false ['flag'] = 0
+
+    df = pd.concat([df_te_true, df_te_false])
+
+    csv = f'{path}/test_data.csv'
+    df.to_csv(csv)
+    
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+
+
+    print(f'processing path = {path}')
+
+    main(path)
